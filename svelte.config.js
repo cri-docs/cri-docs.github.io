817cc8789d55530fa1a54e3e49c56c5b665effb0
jonasvonarb <login@jonasvonarb.ch>
2025-07-15 17:29:33 +0200

    updated stiuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [vitePreprocess()],
 	kit: {
-		preprocess: vitePreprocess(),
 		adapter: adapter({
 			fallback: '404.html',
 		}),
