f8e0d8d0d504ddac618d9e8c3a5e617c15cd3de3
jonasvonarb <login@jonasvonarb.ch>
2025-07-15 17:22:37 +0200

    try othre workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		preprocess: vitePreprocess(),
 		adapter: adapter({
 			fallback: '404.html',
-			pages: 'build/',
-			assets: 'build/',
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH 
 		}
 	}
 };
