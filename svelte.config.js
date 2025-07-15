d8b2e1961c13808a4d9e1a471cfdf9351ef8f99b
jonasvonarb <login@jonasvonarb.ch>
2025-07-15 17:13:29 +0200

    added .nojeckyll

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,16 @@
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(
-			{
-				fallback: '200.html' // may differ from host to host
-			}
-			// {
-			// 	// default options are shown. On some platforms
-			// 	// these options are set automatically — see below
-			// 	pages: 'build',
-			// 	assets: 'build',
-			// 	fallback: undefined,
-			// 	precompress: false,
-			// 	strict: true
-			// }
-		)
+		adapter: adapter({
+			fallback: '404.html',
+			pages: 'build/',
+			assets: 'build/',
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
